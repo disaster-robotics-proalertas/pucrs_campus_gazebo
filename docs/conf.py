372da948df098c2a6d4b2f4d4e3eed82a7aa6889
Alexandre Amory <amamory@users.noreply.github.com>
2017-12-20 11:32:01 -0200

    change doc name

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'platypus_boat_doc'
+project = 'PUCRS Campus on Gazebo'
 copyright = '2017, Alexandre Amory'
 
 # The version info for the project you're documenting, acts as replacement for
